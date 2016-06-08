38454fa1c9cdedd09e940d961fc17ca3ed232dfa
Daocun Yang <ydcdar@gmail.com>
2016-06-08 15:21:34 +0800

    Update server-info.sh

--- server-info.sh
+++ server-info.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
+# A script to collect some basic info (e.g. CPU, memory, IP address, etc) from servers. 
+
 echo "" > ./info.txt
-for i in 127.0.0.1;  # replace with your IP addresses
+for i in 127.0.0.1;  # replace with your IP addresses, separated by a space
 do
   ssh root@$i "hostname" >> ./info.txt
   ssh root@$i "/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{print $1}'" >> ./info.txt
@@ -10,7 +12,7 @@ do
   echo " " >> ./info.txt
   ssh root@$i "df -h | awk 'FNR==4{print $2}' " >> ./info.txt
   echo " " >> ./info.txt
-  ssh root@$i "ps -ef | grep java" >> ./info.txt
+  ssh root@$i "ps -ef | grep java" >> ./info.txt   # can be other processes
   echo "" >> ./info.txt
   echo "--------------------------------------------------" >> ./info.txt
 done

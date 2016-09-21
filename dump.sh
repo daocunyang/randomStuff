0901e34276268a1b483fcdbcc4fed55bfda62af8
Daocun Yang <Daocun@Daocuns-MacBook-Pro.local>
2016-09-21 12:28:31 -0500

    add dump script

---
+++ dump.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# a script to compile assembly code and dump it into hex
+# for one of the tasks in MP1 in CS461
+
+echo "Starting..."
+rm -f tcp.txt
+gcc -c tcp.S
+objdump -d tcp.o > tcp.txt
+echo "Done." 
